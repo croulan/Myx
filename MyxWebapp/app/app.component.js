bec97951f79b1e92c7fb73ecbb3a053eb98e06bf
Roulan Ceniza <roulanceniza@Users-MacBook-Pro.local>
2016-10-22 00:48:18 -0500

    Sample mobile webapp

---
+++ MyxWebapp/app/app.component.js
@@ -0,0 +1,15 @@
+
+(function(app) {
+    app.AppComponent =
+        ng.core.Component({
+            selector: 'my-app',
+            template: '<h1>My First Angular App</h1>'
+
+        })
+    .Class({
+        constructor: function() {}
+
+    });
+
+})(window.app || (window.app = {}));
+
---
+++ MyxWebapp/app/main.js
@@ -0,0 +1,11 @@
+
+(function(app) {
+    document.addEventListener('DOMContentLoaded', function() {
+        ng.platformBrowserDynamic
+            .platformBrowserDynamic()
+            .bootstrapModule(app.AppModule);
+
+    });
+
+})(window.app || (window.app = {}));
+
